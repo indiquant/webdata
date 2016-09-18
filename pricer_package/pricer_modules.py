024150a1b61606d074e3a1b5aae17c8587e68b3f
oneashok1 <am3992@columbia.edu>
2016-09-17 23:03:05 -0400

    still having convergence problem

--- pricer_package/pricer_modules.py
+++ pricer_package/pricer_modules.py
@@ -13,10 +13,10 @@ from  scipy.stats import norm
 import datetime as dt
 
 def find_vol(target_value, call_put, S, K, T, r):
-    MAX_ITERATIONS = 10
+    MAX_ITERATIONS = 100
     PRECISION = 1.0e-5
 
-    sigma = 0.5
+    sigma = 2
     for i in np.arange(0, MAX_ITERATIONS):
         price = bs_price(call_put, S, K, T, r, sigma)
         vega = bs_vega(call_put, S, K, T, r, sigma)
@@ -30,6 +30,8 @@ def find_vol(target_value, call_put, S, K, T, r):
             return sigma
         sigma = sigma + diff/vega # f(x) / f'(x)
 
+        if sigma<=0:
+            sigma = 0.5
     # value wasn't found, return best guess so far
     return sigma
 
@@ -38,7 +40,7 @@ n = norm.pdf
 N = norm.cdf
 
 def bs_price(cp_flag,S,K,T,r,v,q=0.0):
-    d1 = (np.log(S/K)+(r+v*v/2.)*T)/(v*np.sqrt(T))
+    d1 = (np.log(S/K)+(r+v*v/2.0)*T)/(v*np.sqrt(T))
     d2 = d1-v* np.sqrt(T)
     if cp_flag == 'c':
         price = S* np.exp(-q*T)*N(d1)-K*np.exp(-r*T)*N(d2)
@@ -47,7 +49,7 @@ def bs_price(cp_flag,S,K,T,r,v,q=0.0):
     return price
 
 def bs_vega(cp_flag,S,K,T,r,v,q=0.0):
-    d1 = (np.log(S/K)+(r+v*v/2.)*T)/(v*np.sqrt(T))
+    d1 = (np.log(S/K)+(r+v*v/2.0)*T)/(v*np.sqrt(T))
     return S * np.sqrt(T)*n(d1)
 
 if __name__ == '__main__':
@@ -67,17 +69,22 @@ if __name__ == '__main__':
     #print(input_df.info() )
     
     
-    r = 0.002
+    r = 0.02
     
-    for _,row in input_df.head(1).iterrows():
+    for _,row in input_df.head(10).iterrows():
         #print (row)
+        '''
         print (np.nanmean([ row['bidpx'], row['askpx'] ], axis=0),
                    row['opt_type'], row['spot'], row['strike'], 
                 row['time_to_expiry'], r)
+        '''
+        if pd.isnull(np.nanmean([ row['bidpx'], row['askpx'] ],axis=0) ):
+            continue
+
         vol = find_vol( np.nanmean([ row['bidpx'], row['askpx'] ], axis=0),
                    row['opt_type'], row['spot'], row['strike'], 
                 row['time_to_expiry'], r)
-        print (vol)
+        print (vol * 100)
         
     
     
